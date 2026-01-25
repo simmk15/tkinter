fca19e8059f0612f43650299a67445a6d6c110b9
simmk15 <sknagra15@gmail.com>
2026-01-25 21:03:22 +0530

    Add files via upload

---
+++ Restraunt_management_system.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+from tkinter import ttk,messagebox
+class RestrauntOrderManagement:
+    def __init__(self,root): 
+        self.root=root
+        self.root.title=("Restraunt Management App")
+        self.menu_items={
+            "FRIES MEAL":2,
+            "LUNCH MEAL":2,
+            "BURGER MEAL":3,
+            "PIZZA MEAL":4,
+            "CHEESE BURGER":2.5,
+            "DRINKS":1
+        }
+        self.exchange_rate=82
+        self.setup_background(root)
+        frame=ttk.Frame(root)
+        frame.place(relx=0.5,rely=0.5,anchor=tk.CENTER)
+        ttk.Label(frame,text="Restraunt Order Management",font=("Arial",20,"bold")).grid(row=0,columnspan=3,padx=10,pady=10)
+        self.menu_labels={}
+        self.menu_quantities={}
+        for i,(item,price)in enumerate(self.menu_items.items(),start=1):
+            label=ttk.Label(frame,text=f"{item}(${price}):",font=("Arial",12))
+            label.grid(row=i,column=0,padx=10,pady=5)
+            self.menu_quantities[item]=quantity_entry
+        self.currency_var=tk.StringVar()
+        ttk.Label(frame,text="Currency",font=("Arial",12)).grid(row=len(self.menu_items)+1,column=0,padx=10,pady=5)
+        currency_dropdown=ttk.Combobox(frame,textvariable=self.curency_var,state="readonly",width=18,values=("USD","INR"))
+        currency_dropdown.grid(row=len(self.menu_items)+1,column=1,padx=10,pady=5)
+        currency_dropdown.current(0)
+        self.currency_var.trace("w",self.update_menu_prices)
+        order_button=ttk.Button(frame,text="Place Order",command=self.place_order)
+        order_button.grid(row=len(self.menu_items)+2,columnspan=3,padx=10,pady=10)
+    def setup_background(self,root):
+        bg_width,bg_height=800,600
+        canvas=tk.Canvas(root,width=bg_width,height=bg_height)
+        canvas.pack()
+        original_image=tk.PhotoImage(file="background.png")
+        background_image=original_image.subsample(original_image.width()//bg_width,original_image.height()//bg_height)
+        canvas.create_image(0,0,anchor=tk.NW,image=background_image)
+        canvas.image=background_image
+    def update_menu_prices(self,*args):
+        currency=self.currency_var.get()
+        symbol="₹"if currency=="INR"else"$"
+        rate=self.exchange_rate if currency=="INR" else 1
+        for item,label in self.menu_labels.items:
+            price=self.menu_items[item]*rate
+            label.config(text=)
